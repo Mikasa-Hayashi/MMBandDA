a71476c6a92c20bba65823bbb1341a4667e7ef0b
Starikov Ilya <cubic210705@gmail.com>
2025-09-22 20:29:49 +0300

    Добавил задание 17.3

---
+++ lab2/task17.3.py
@@ -0,0 +1,32 @@
+import sympy as sp
+
+def derivative(f, x):
+    for n in range(1, 4):
+        deriv = sp.diff(f, x, n)
+        print(f"\nПроизводная {n}-го порядка:")
+        print(deriv)
+
+# Определяем символьную переменную
+x = sp.Symbol('x', real=True, positive=True)    # real=True означает, что x - вещественное число
+                                                # positive=True означает, что x > 0
+
+# Функция a
+print("Fanction a: ")
+# print("=" * 60)
+
+f_a = 3*sp.exp(-2*x) + (x**(1/3)) / sp.log(sp.log(x))
+print("f(x) =", f_a)
+
+# Производные
+derivative(f_a, x)
+
+# Функция b
+print("\n" * 2)
+print("Fanction b: ")
+
+f_b = (7*x**4 + sp.cos(1-x)) / sp.asin(x)
+
+print("f(x) =", f_b)
+
+# Производные
+derivative(f_b, x)
